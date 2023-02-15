f4275b41438c43502b76c18ee3bfd8863026c4bf
Waluyo Ade Prasetio <3275090110970014>
2023-02-15 16:34:09 +0700

    add feature check version and update

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/abdullahPrasetio/go-smart-gen
 go 1.18
 
 require (
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/jessevdk/go-assets v0.0.0-20160921144138-4f4301a06e15 // indirect
 	github.com/jessevdk/go-assets-builder v0.0.0-20130903091706-b8483521738f // indirect
 	github.com/jessevdk/go-flags v1.5.0 // indirect
+	github.com/tcnksm/go-latest v0.0.0-20170313132115-e3007ae9052e // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 )
--- smartgen.go
+++ smartgen.go
@@ -4,22 +4,41 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/abdullahPrasetio/go-smart-gen/generator"
+	"github.com/tcnksm/go-latest"
 )
 
+const TagVersion = "v1.0.0"
+
 var (
-	make   = flag.String("make", "", "Please write make")
-	folder = flag.String("folder", "", "Please write folder")
+	make       = flag.String("make", "", "Please write make")
+	folder     = flag.String("folder", "", "Please write folder")
+	version    = flag.Bool("version", false, "Please write version")
+	update     = flag.Bool("update", false, "Please write update")
+	newVersion = flag.String("newversion", "", "Please write update")
 )
 
 func main() {
 	flag.Parse()
-	if *make == "" {
-		fmt.Println("Error: Parameter --make harus ditentukan")
-		os.Exit(1)
-	}
+	// if *make == "" {
+	// 	fmt.Println("Error: Parameter --make harus ditentukan")
+	// 	os.Exit(1)
+	// }
 	makeFile := *make
+
+	if *version {
+		// fmt.Println(*version)
+		// GetCurrentVersion()
+		CheckVersion()
+	}
+	if *update {
+		newVersioning := *newVersion
+		UpdateVersion(newVersioning)
+	}
+
 	switch makeFile {
 	case "model":
 		generator.CreateEntityV2()
@@ -43,6 +62,73 @@ func main() {
 		// 	},
 		// }
 		// generator.CreateService("User", fields)
-		generator.CreateEntityV2()
+		// generator.CreateEntityV2()
+		fmt.Println("Welcome to smartgen")
 	}
+}
+
+func CheckVersion() {
+	githubTag := &latest.GithubTag{
+		Owner:      "abdullahPrasetio",
+		Repository: "go-smart-gen",
+	}
+	currentVersion := GetCurrentVersion()
+	// Periksa apakah versi terbaru tersedia.
+	res, err := latest.Check(githubTag, currentVersion)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	newVersion := "v" + res.Current
+	// fmt.Println(res)
+
+	// Jika versi terbaru tersedia, berikan tahu pengguna.
+	if res.Outdated {
+		fmt.Printf("Versi terbaru %s tersedia! (Anda menggunakan versi %s)\n", newVersion, currentVersion)
+		fmt.Println("Silahkan jalankan program dan tambahkan --update untuk melakukan update generator")
+	} else {
+		fmt.Printf("Anda menggunakan versi terbaru %s\n", newVersion)
+	}
+}
+
+func UpdateVersion(newVersion string) {
+	version := "latest"
+	if newVersion != "" {
+		version = newVersion
+	}
+	fmt.Println(version)
+	cmd := exec.Command("go", "install", "github.com/abdullahPrasetio/go-smart-gen@"+version)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+	fmt.Println("sukses update versi")
+	return
+}
+
+func GetCurrentVersion() string {
+	// nama modul yang ingin diperiksa
+	moduleName := "github.com/abdullahPrasetio/go-smart-gen"
+
+	// jalankan perintah 'go list -m' dengan nama modul
+	cmd := exec.Command("go", "list", "-m", "-versions", moduleName)
+	output, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return ""
+	}
+
+	// baca output sebagai string
+	outputString := string(output)
+
+	// parsing output untuk mendapatkan versi
+	fields := strings.Fields(outputString)
+	// fmt.Println(outputString)
+	version := fields[len(fields)-1]
+
+	// tampilkan versi
+	// fmt.Println("Versi", moduleName, "yang terinstal:", version)
+	return version
+
 }
